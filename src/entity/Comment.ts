10056d311c745c92e07f0f91a7f949ab94381703
gloriacc <xuan_1213@yeah.net>
2020-07-14 15:47:26 +0800

    create data association

---
+++ src/entity/Comment.ts
@@ -0,0 +1,19 @@
+import {Column, CreateDateColumn, Entity, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn} from 'typeorm';
+import {User} from './User';
+import {Post} from './Post';
+
+@Entity()
+export class Comment {
+  @PrimaryGeneratedColumn('increment')
+  id: number;
+  @Column('text')
+  content: string;
+  @CreateDateColumn('time')
+  createdAt: Date;
+  @UpdateDateColumn('time')
+  updatedAt: Date;
+  @ManyToOne(type => User, user => user.comments)
+  user: User;
+  @ManyToOne(type => Post, post => post.comments)
+  post: Post;
+}
