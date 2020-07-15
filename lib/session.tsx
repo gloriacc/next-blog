be6fe8cea765ea7063e853bc1c40e1625e3a720e
gloriacc <xuan_1213@yeah.net>
2020-07-15 16:20:14 +0800

    .env.local 设置环境变量

--- lib/session.tsx
+++ lib/session.tsx
@@ -3,7 +3,7 @@ import {withIronSession} from 'next-iron-session';
 
 export function withSession(handler: NextApiHandler) {
   return withIronSession(handler, {
-    password: '0e07843d-ad82-4dd1-8452-288d58b5b025',
+    password: process.env.SECRET,
     cookieName: 'blog',
     cookieOptions: {
       secure: false
