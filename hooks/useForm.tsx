d06e2481f4c24dc73492c2c3c550c74b0870fe94
gloriacc <xuan_1213@yeah.net>
2020-07-16 14:27:23 +0800

    提交 post

--- hooks/useForm.tsx
+++ hooks/useForm.tsx
@@ -49,10 +49,10 @@ export function useForm<T>(options: UseFormOptions<T>) {
   const form = (
     <form onSubmit={onSubmit}>
       {fields.map(field =>
-        <div>
+        <div key={field.key.toString()}>
           <label>{field.label}
             {field.type === 'textarea' ?
-              <textarea onChange={e => onChange(field.key, e.target.value)}>{formData[field.key].toString()}</textarea>
+              <textarea onChange={e => onChange(field.key, e.target.value)} value={formData[field.key].toString()}/>
               :
               <input type={field.type} value={formData[field.key].toString()} onChange={e => onChange(field.key, e.target.value)}/>
             }
--- pages/api/v1/posts.tsx
+++ pages/api/v1/posts.tsx
@@ -1,12 +1,18 @@
 import {NextApiHandler} from 'next';
-import {getPosts} from '../../../lib/posts';
+import {Post} from '../../../src/entity/Post';
+import {getDatabaseConnection} from '../../../lib/getDatabaseConnection';
+import {withSession} from '../../../lib/session';
 
-const Posts:NextApiHandler = async (req, res) => {
-  const posts = await getPosts();
-  console.log(posts);
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'application/json');
-  res.write(JSON.stringify(posts));
-  res.end();
-};
+const Posts:NextApiHandler =  withSession(async (req, res) => {
+  if (req.method === 'POST') {
+    const {title, content} = req.body;
+    const post = new Post();
+    post.title = title;
+    post.content = content;
+    post.author = req.session.get('currentUser');
+    const connection = await getDatabaseConnection();
+    await connection.manager.save(post);
+    res.json(post);
+  }
+});
 export default Posts;
